de4b8b1a298a055d844c0e2b297ea0a1ebc64296
Amy Allen <amyallen8120@live.com>
2017-09-22 15:49:18 -0400

    Update 404.php

--- aea-portfolio/404.php
+++ aea-portfolio/404.php
@@ -12,41 +12,38 @@
 <div class="spinner-container"><div class="spinner"></div></div>
 
 <div class="page-wrapper">
-
-<header>
-    <div class="header__container">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><h3 id="logo" class="header__logo">Amy Allen</h3></a>
-        <nav class="header__nav">
-      <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'fallback_cb' => false ) ); ?>
-        </nav>
-    </div>
-</header>
-
-<section class="hero-banner">
-  <div class="hero-banner__wrapper">
-
-    <div class="hero-banner__main-heading">
-      
-      <h1>Oops<span>!</span> We can't seem to find the page you're looking for<span>.</span></h1>
-
-    </div>
-
-    <h2>Error code: 404</h2>
-  </div>  
-
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-   viewBox="0 0 144.7059937 156.4597778" style="enable-background:new 0 0 144.7059937 156.4597778;" xml:space="preserve">
-<g id="Layer_1">
-  <polyline class="st0" points="142.2059937,78.9597778 107.2794952,116.4597778 72.3529968,153.9597778 37.4264984,116.4597778 
-    2.5,78.9597778  "/>
-  <polyline class="st0" points="128.2353973,54.5431099 100.2941895,84.5431061 72.3529968,114.5431061 44.4117966,84.5431061 
-    16.4705982,54.5431099   "/>
-  <polyline class="st0" points="114.2647934,30.1264439 93.3088913,52.626442 72.3529968,75.126442 51.3970947,52.626442 
-    30.4411983,30.1264439   "/>
-</g>
-</svg>
-
   
-</section><!-- Hero Banner Section -->
+  <div class="content-wrapper"> 
+
+    <div class="sidebar-image"></div>
+    
+    <div class="page-content">
+      
+      <div class="header-wrapper">
+
+        <header>
+            <div class="header__container">
+                <a class="header__logo-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><h3 id="logo" class="header__logo">amy allen<span>.</span></h3></a>
+                <nav class="header__nav">
+              <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'fallback_cb' => false ) ); ?>
+                </nav>
+            </div>
+        </header>
+        
+        <div class="header__content">
+          
+          <div class="header__background-overlay"></div>
+
+          <div class="header__content-text-ctn">
+
+            <h1>Oops<span>!</span> I can't seem to find the page you're looking for<span>.</span></h1>
+
+            <h2>Error code: 404</h2>
+              
+          </div>
+
+          </div>
+
+      </div>
 
 <?php get_footer(); ?>
